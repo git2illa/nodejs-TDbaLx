ceff4c19db77a74a740fc5d8b976f7300b787790
catzillaorz <catzillaorz@outlook.com>
2024-06-20 16:54:25 +0800

    feat: 04_01_spider_step1

---
+++ 04-01-spider/spider.js
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import path from 'path';
+import superagent from 'superagent'
+import mkdirp from 'mkdirp'
+import {urlToFilename} from './utils.js'    
+
+export function sipider(url, cb){
+  const filename = urlToFilename(url);
+  fs.access(filename, err => {
+    if(err && err.code === "ENOENT"){
+      console.log(`Downloading${url} into ${filename}`)
+      superagent.get(url).end((err, res)=> {
+        if(err){
+          cb(err)
+        }else{
+          mkdirp(path.dirname(filename), err => {
+            if(err){
+              cb(err)
+            }else{
+              fs.writeFile(filename, res.text, err => {
+                if(err){
+                  cb(err)
+                } else{
+                  cb(null, filename, true)
+                }
+              })
+            }
+          })
+        }
+      })
+    }
+  })
+}
