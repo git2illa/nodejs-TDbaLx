7c9bc13ef6784599c1fdd1260c03efd59031ee41
catzillaorz <catzillaorz@outlook.com>
2024-06-21 15:59:47 +0800

    feat: refactor callback | basic principles

--- 04-01-spider/spider.js
+++ 04-01-spider/spider.js
@@ -7,29 +7,40 @@ import {urlToFilename} from './utils.js'
 export function spider(url, cb){
   const filename = urlToFilename(url);
   fs.access(filename, err => {
-    if(err && err.code === "ENOENT"){
-      console.log(`Downloading ${url} into ${filename}`)
-      superagent.get(url).end((err, res)=> {
-        if(err){
-          cb(err)
-        }else{
-          mkdirp(path.dirname(filename), err => {
-            if(err){
-              cb(err)
-            }else{
-              fs.writeFile(filename, res.text, err => {
-                if(err){
-                  cb(err)
-                } else{
-                  cb(null, filename, true)
-                }
-              })
-            }
-          })
-        }
-      })
-    } else{
-      cb(null, filename, false)
+    if(!err || err.code !== "ENOENT"){
+      return cb(null, filename, false)
     }
+    downloadFile(url, filename, err =>{
+      if(err){
+        return cb(err)
+      }
+      cb(null, filename, true)
+    })
+  })
+}
+
+function downloadFile(url, filename, cb){
+  console.log(`Downloading ${url}`)
+  superagent.get(url).end((err, res)=> {
+    if(err){
+      return cb(err)
+    }
+    saveFile(filename, res.text, err=>{
+      if(err){
+        return cb(err)
+      }
+      console.log(`Downloading and saved: ${url}`)
+      cb(null, res.text)
+    })
+  })
+
+}
+
+function saveFile(filename, contents, cb){
+  mkdirp(path.dirname(filename), err => {
+    if(err){
+      return cb(err)
+    }
+    fs.writeFile(filename, contents, cb)
   })
 }
